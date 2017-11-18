81c51a8f711116cb6062205e064ba0092ae7a315
Hilman Maulana <hilman0.0maulana@gmail.com>
2017-11-18 17:15:54 +0700

    Update Manga Reader ver 1.2.js

--- Manga Reader/Manga Reader ver 1.2.js
+++ Manga Reader/Manga Reader ver 1.2.js
@@ -1,6 +1,6 @@
 //<![CDATA[
 //////////////////////////////////////
-// Manga Reader ver 1.2 Final for blogger
+// Manga Reader ver 1.2 beta for blogger
 // Compiled Javascript (from any resource)
 // License Creative Commons Attribution-Noncommercial-Share Alike 3.0 License.
 // Free Copy, Free Use, Free Modification, Give proper credit
